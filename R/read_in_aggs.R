fee44b8568db6c59c9948dbf36a38a73457ff330
Galen Holt <53928785+galenholt@users.noreply.github.com>
2024-07-19 16:36:51 +1000

    calculating vulnerability

--- R/read_in_aggs.R
+++ R/read_in_aggs.R
@@ -1,16 +1,101 @@
 read_in_aggs <- function(project_dir,
                          data_path = NULL,
-                            data_type = c('historical', 'stochastic'),
-                            mark = 'all',
-                            licvol = 'all',
-                            climate = 'all',
-                            aggname = 'all',
-                            lastagg,
-                            retain_cols) {
+                         lastagg,
+                         retain_cols,
+                         data_type = c('historical', 'stochastic'),
+                         mark = 'all',
+                         licvol = 'all',
+                         climate = 'all',
+                         aggname = 'all') {
 
-
-  # construct requested data paths if not given explicitly
   if (is.null(data_path)) {
+    data_path <- get_scenariodir(project_dir = project_dir,
+                                   data_type = data_type,
+                                   mark = mark,
+                                   licvol = licvol,
+                                   climate = climate,
+                                   aggname = aggname)
+  }
+
+  fullpaths <- file.path(project_dir, 'aggregated', data_path, 'achievement_aggregated.rds')
+
+
+  # read in the files
+  clean_aggs <- purrr::map(fullpaths, readRDS)
+
+  # should put a test in here that all the retain_cols are actually in the df
+
+
+  # Clean them up and combine
+  clean_aggs <- purrr::map2(clean_aggs, data_path,
+                            \(x,y) clean_aggregated(x,
+                                                    lastagg = lastagg,
+                                                    retain_cols = retain_cols,
+                                                    subdir = y)) |>
+    dplyr::bind_rows()
+
+  return(clean_aggs)
+
+}
+
+
+select_sequence <- function(aggdata, lastagg, retain_cols) {
+  # aggregated columns will always start with the last aggsequence name
+  not_aggs <- which(!grepl(paste0('^',
+                                  lastagg,
+                                  '_'), x = names(aggdata)))
+
+  desired_aggs <- tidyselect::eval_select(retain_cols, aggdata)
+
+  aggdata <- aggdata |>
+    dplyr::select(tidyselect::all_of(c(not_aggs, desired_aggs)))
+
+  return(aggdata)
+}
+
+
+clean_aggregated <- function(oneagg, lastagg, retain_cols, subdir) {
+
+
+  oneagg <- select_sequence(aggdata = oneagg, lastagg, retain_cols)
+
+  # If we keep MAX, will need to be careful on read-in of multiple agg files not to duplicate it.
+
+  # Make scenarios unique
+  # I *hate* using path separators / in this, but if I use _ like I want, it'll get confused with Ash's use to sub them with . in things like 1_07.
+
+  # I should parse here too into the cols to actually ID them
+  fix_ <- function(x) {
+    x <- gsub('[A-Za-z]*', '', x)
+    x <- gsub('^_', '', x)
+    x <- gsub('_', '.', x)
+    x <- as.numeric(x)
+  }
+
+  oneagg <- oneagg |>
+    dplyr::mutate(scenario = paste0(subdir, '/', scenario)) |>
+    tidyr::separate_wider_delim(scenario, '/',
+                                names = c('Data', 'Mk', 'licvolfactor',
+                                          'rem', 'aggregation', 'stoch_replicate'),
+                                cols_remove = FALSE) |>
+    tidyr::separate_wider_delim(rem, "_e",
+                                names = c("Rainfall","Evapotranspiration")) |>
+    dplyr::mutate(across(c(licvolfactor, Rainfall, Evapotranspiration), fix_)) |>
+    # not sure about this; it's Cudgegong NAs
+    dplyr::filter(!is.na(polyID)) |>
+    dplyr::select(scenario, tidyselect::everything()) |>
+    dplyr::relocate(geometry, .after = last_col())
+
+  return(oneagg)
+}
+
+get_scenariodir <- function(project_dir,
+                              data_type = c('historical', 'stochastic'),
+                              mark = 'all',
+                              licvol = 'all',
+                              climate = 'all',
+                              aggname = 'all') {
+
     # assume only one project_dir
 
     # keep working on this stuff as the aggs run.
@@ -70,69 +155,5 @@ read_in_aggs <- function(project_dir,
 
     data_path <- purrr::map2(data_path, aggname_list, file.path) |> unlist()
 
-  }
-
-
-  paths_to_files <- file.path(project_dir, 'aggregated', data_path, 'achievement_aggregated.rds')
-
-
-
-  clean_aggs <- purrr::map(paths_to_files, readRDS)
-
-  # should put a test in here that all the retain_cols are actually in the df
-
-
-  # overwrite to save memory
-  clean_aggs <- purrr::map2(clean_aggs, data_path,
-                            \(x,y) clean_aggregated(x,
-                                                    lastagg = lastagg,
-                                                    retain_cols = retain_cols,
-                                                    subdir = y)) |>
-    dplyr::bind_rows()
-
-  return(clean_aggs)
-
-}
-
-
-clean_aggregated <- function(oneagg, lastagg, retain_cols, subdir) {
-
-  # aggregated columns will always start with the last aggsequence name
-  not_aggs <- which(!grepl(paste0('^',
-                                  lastagg,
-                                  '_'), x = names(oneagg)))
-
-  desired_aggs <- tidyselect::eval_select(retain_cols, oneagg)
-
-  oneagg <- oneagg |>
-    dplyr::select(tidyselect::all_of(c(not_aggs, desired_aggs)))
-
-  # If we keep MAX, will need to be careful on read-in of multiple agg files not to duplicate it.
-
-  # Make scenarios unique
-  # I *hate* using path separators / in this, but if I use _ like I want, it'll get confused with Ash's use to sub them with . in things like 1_07.
-
-  # I should parse here too into the cols to actually ID them
-  fix_ <- function(x) {
-    x <- gsub('[A-Za-z]*', '', x)
-    x <- gsub('^_', '', x)
-    x <- gsub('_', '.', x)
-    x <- as.numeric(x)
-  }
-
-  oneagg <- oneagg |>
-    dplyr::mutate(scenario = paste0(subdir, '/', scenario)) |>
-    tidyr::separate_wider_delim(scenario, '/',
-                                names = c('Data', 'Mk', 'licvolfactor',
-                                          'rem', 'aggregation', 'stoch_replicate'),
-                                cols_remove = FALSE) |>
-    tidyr::separate_wider_delim(rem, "_e",
-                                names = c("Rainfall","Evapotranspiration")) |>
-    dplyr::mutate(across(c(licvolfactor, Rainfall, Evapotranspiration), fix_)) |>
-    # not sure about this; it's Cudgegong NAs
-    dplyr::filter(!is.na(polyID)) |>
-    dplyr::select(scenario, tidyselect::everything()) |>
-    dplyr::relocate(geometry, .after = last_col())
-
-  return(oneagg)
+  return(data_path)
 }
